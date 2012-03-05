f426673d92a6ff823743e497922d2cd066ae698b
Radu Ioan Fericean <radu@fericean.ro>
2012-03-05 19:31:28 +0200

    added search

--- controllers/default.py
+++ controllers/default.py
@@ -9,9 +9,20 @@
 ## - call exposes all registered services (none by default)
 #########################################################################
 
-def index():    
+def index(): 
     sounds = db(db.sounds).select()
+    form = SQLFORM.factory(Field('query', default=T('Search')), _action=URL('search'))        
     return locals()
+
+def search():
+    form = SQLFORM.factory(Field('query', default=T('Search')), _action=URL('search'))
+    query = db.sounds
+    if form.process().accepted and form.vars.query:
+        values = form.vars.query.split(' ')        
+        query = db.sounds.title.contains(values, False)
+        query |= db.sounds.description.contains(values, False)            
+    sounds = db(query).select()
+    return response.render('default/index.html', locals())
 
 @auth.requires_login()
 def upload():
@@ -23,7 +34,7 @@ def details():
         raise HTTP(404)
     user = db.auth_user(sound.created_by)
     if not user:
-        user = T("Annonimous")
+        user = T("Anonymous")
     else:
         user = user.first_name + " " + user.last_name
     return locals()
