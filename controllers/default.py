d49686641724e2b1d9bc291b274f6cc6c2334e21
Radu Fericean <radu@fericean.ro>
2012-03-15 15:15:03 +0200

    better sound updating

--- controllers/default.py
+++ controllers/default.py
@@ -3,9 +3,7 @@ from plugin_paginator import Paginator, PaginateSelector, PaginateInfo
 
 def index():
     paginate_selector = PaginateSelector(anchor='main')
-    paginator = Paginator(paginate=paginate_selector.paginate, 
-                          extra_vars={'v':1}, anchor='main',
-                          renderstyle=True) 
+    paginator = Paginator(paginate=paginate_selector.paginate,  extra_vars={'v':1}, anchor='main', renderstyle=True) 
     paginator.records = db(active_sounds).count()
     paginate_info = PaginateInfo(paginator.page, paginator.paginate, paginator.records)
     
@@ -14,7 +12,8 @@ def index():
     sounds = None
     if form.process(message_onsuccess="").accepted and form.vars.query:
         values = form.vars.query        
-        sounds = db(active_sounds).select(orderby=~db.sounds.created_on, limitby=paginator.limitby()).find(lambda s: values.lower() in s.title.lower() or values.lower() in s.description.lower() or values.lower() in s.keywords.lower())
+        sounds = db(active_sounds).select(orderby=~db.sounds.created_on,
+            limitby=paginator.limitby()).find(lambda s: values.lower() in s.title.lower() or values.lower() in s.description.lower() or values.lower() in s.keywords.lower())
     else:
         sounds = db(active_sounds).select(orderby=~db.sounds.created_on, limitby=paginator.limitby())
     return locals()
@@ -44,31 +43,33 @@ def blobstore_upload(form):
 @auth.requires_login()
 def create_sound():
     form=crud.create(db.sounds, onvalidation=blobstore_upload, next=URL('my_uploads', user_signature=True), message=T('Upload complete!'))
-    return response.render('default/upload.html', locals())
+    return locals()
 
 @auth.requires_login()
 @auth.requires_signature()
-def update_sound():
-    form=crud.update(db.sounds, a0, onvalidation=blobstore_upload, next=URL('my_uploads', user_signature=True), message=T('Upload complete!'))
-    return response.render('default/upload.html', locals())
+def update_sound():    
+    sound = db.sounds(a0) or redirect(URL('index'))
+    form = SQLFORM(db.sounds, sound, fields=['title', 'description', 'keywords', 'language', 'price'], showid=False)
+    if form.process().accepted:
+       response.flash = T('Sound info updated!')
+       redirect(URL('my_uploads', user_signature=True))
+    elif form.errors:
+       response.flash = T('form has errors')    
+    return locals()
 
 @auth.requires_login()
 @auth.requires_signature()
 def delete_sound():
-    from google.appengine.ext import blobstore
-    sound = db.sounds(a0)
-    if not sound:
-        raise HTTP(404)
+    from google.appengine.ext import blobstore    
+    sound = db.sounds(a0) or redirect(URL('index'))
     blobstore.delete_async(sound.blob_key)
-    crud.delete(db.sounds, a0, next=URL('my_uploads'), message=T('Sound deleted!'))
+    crud.delete(db.sounds, a0, next=URL('my_uploads', user_signature=True), message=T('Sound deleted!'))
     return locals()
 
 @auth.requires_login()
 @auth.requires_signature()
 def my_uploads():    
-    paginator = Paginator(paginate=10, 
-                          extra_vars={'v':1}, anchor='main',
-                          renderstyle=True) 
+    paginator = Paginator(paginate=10, extra_vars={'v':1}, anchor='main', renderstyle=True) 
     paginator.records = db(user_sounds).count()
     paginate_info = PaginateInfo(paginator.page, paginator.paginate, paginator.records)
 
@@ -76,25 +77,25 @@ def my_uploads():
     return locals()
 
 def details():    
-    detail_sound = db.sounds(a0)    
-    if not detail_sound:
-        raise HTTP(404)    
+    detail_sound = db.sounds(a0) or redirect(URL('index'))   
     query = active_sounds & (db.sounds.created_by==detail_sound.created_by)
     new_count = detail_sound.play_count or 0 + 1
     detail_sound.update_record(play_count=new_count)
 
     paginate_selector = PaginateSelector(anchor='main')
-    paginator = Paginator(paginate=paginate_selector.paginate, 
-                          extra_vars={'v':1}, anchor='main',
-                          renderstyle=True) 
+    paginator = Paginator(paginate=paginate_selector.paginate, extra_vars={'v':1}, anchor='main', renderstyle=True) 
     paginator.records = db(query).count()
     paginate_info = PaginateInfo(paginator.page, paginator.paginate, paginator.records)
 
     sounds = db(query).select(orderby=~db.sounds.created_on, limitby=paginator.limitby())
     return locals()
 
-def most_popular():              
-    sounds = db(active_sounds).select(orderby=~db.sounds.play_count, limitby=10)
+def most_popular():        
+    paginate_selector = PaginateSelector(anchor='main')
+    paginator = Paginator(paginate=paginate_selector.paginate, extra_vars={'v':1}, anchor='main', renderstyle=True) 
+    paginator.records = db(active_sounds).count()    
+
+    sounds = db(active_sounds).select(orderby=~db.sounds.play_count, limitby=paginator.limitby())
     return locals()
 
 def user():
@@ -107,9 +108,7 @@ def download():
 def download_blob():    
     if request.env.web2py_runtime_gae: 
         from google.appengine.ext import blobstore        
-        sound = db.sounds(a0)    
-        if not sound:
-            raise HTTP(404)
+        sound = db.sounds(a0) or redirect(URL('index'))
         blob_info = blobstore.BlobInfo.get(sound.blob_key)
         return response.stream(blob_info.open())                
     else:        
