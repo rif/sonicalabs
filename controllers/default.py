257c56611c481cf7fd04ebac794acc7f71563e3b
Radu Ioan Fericean <radu@fericean.ro>
2012-08-30 13:08:20 +0300

    stupid index page

--- controllers/default.py
+++ controllers/default.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 from plugin_paginator import Paginator, PaginateSelector, PaginateInfo
 
-def index():
+def index(): return dict()
+
+def index_real():
     paginate_selector = PaginateSelector(anchor='main')
     paginator = Paginator(paginate=paginate_selector.paginate,  extra_vars={'v':1}, anchor='main', renderstyle=True)
     paginator.records = db(active_sounds).count()
